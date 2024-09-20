df907010733d7c89476502c4cc815d666a50b5e8
Zzezl <maxmandarin29@gmail.com>
2024-09-20 20:01:02 +0300

    1234

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-const menuBtn = document.querySelector('.menuToggle');
-const menu = document.querySelector('.menu');
+const BASE_URL = "https://rickandmortyapi.com/api"
+const
 
+const load_chars = async function() {
+    const download_chars = await fetch(`${BASE_URL}/character`)
+        .then(res => res.json())
+        .then(data => console.dir(data.results))
+}
+load_chars()
 
+const renderOnPage = () => {
 
-menuBtn.addEventListener('click', () => {
-
-    const toggleVision = (e) => {
-        e.preventDefault();
-        menu.classList.toggle('hidden');
-        menu.removeEventListener('click', toggleVision);
-        menu.querySelector('.item').style.right = "-300px"
-    }
-
-    menu.classList.toggle('hidden');
-    menu.addEventListener('click', toggleVision);
-    menu.querySelector('.item').style.right = "0px"
-})
+}
